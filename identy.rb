fb64e0212cba3f88ebee8663baed15030911cdc6
mame-n <nakauchi.s@gmail.com>
2020-06-28 08:50:46 +0900

    Adding recusive method by my self instead of */**.

--- identy.rb
+++ identy.rb
@@ -3,44 +3,39 @@ require "DICOM"
 #require "./patient_name_id"
 
 class Identy
-  def initialize( argvs )
-    check_argvs( argvs )
+  def initialize( argvs=[] )
+    @dcm_path, @out_file = initialize_argvs( argvs )
     @results = []
     @dbg_folder_paths = []
   end
 
-  def check_argvs( argvs )
+  def initialize_argvs( argvs )
     if argvs.size == 0
       puts_usage
-      @dcm_folder = nil
+      [nil,nil]
     
-    elsif argvs.size == 1
-      @dcm_folder = argvs[0]
+    elsif argvs.size >= 1
+      dcm_path = File.expand_path( argvs[0] )
+      out_file = dcm_path + "/dicom_info.txt"
+      [dcm_path, out_file]
     end
   end
 
-
   def puts_usage
-    puts "No input file path is specified."
+    puts "No input folder name is specified."
     puts "usage: ruby identy <Folder name>"
   end
 
   def main
-    @dcm_path = File.expand_path(@dcm_folder)
-
 #    keep_dir = Dir.pwd
-
-    @out_file = @dcm_path + "/dicom_info.txt"
-    puts "Output file #{@out_file}"
-
 #    Dir.chdir(@dcm_path)
-    ret = convert_cvs( dcm_dirs( @dcm_path ).flatten )
+    dcm_dirs( @dcm_path )
+    ret = convert_cvs( @results )
+#    ret = convert_cvs( dcm_dirs( @dcm_path ) )
     write_cvs( ret )
-    Dir.chdir(keep_dir)
-    ret
   end
 
-  attr_accessor :dcm_folder, :results, :dbg_folder_paths
+  attr_accessor :dcm_path, :results, :dbg_folder_paths, :out_file
 
   def dcm_dirs( folder_path )
 
@@ -51,15 +46,12 @@ class Identy
       file_path = folder_path + "/" + file_name
 
       if File.file?(file_path)
-        puts "#{file_path}"
         dcm = DICOM::DObject.read( file_path )
         if dcm.read?
           @results << [ dcm["0010,0010"].value, dcm["0010,0020"].value, dcm["0008,0020"].value, folder_path ]
-          puts "Result update #{@results}"
-          return 
         else
           # Non DICOM file. Just through this file
-        end          
+        end
 
       elsif Dir.exist?(file_path)
         puts "#{file_path} is folder"; @dbg_folder_paths << file_path
@@ -76,6 +68,7 @@ class Identy
   end
 
   def convert_cvs(dcm_results)
+    puts "@@@@@@@ #{dcm_results}"
     dcm_results.uniq!
 #    puts "Before #{dcm_results}"
     dcm_results.inject(["Patient name,Patient ID,Study Date,DCM File name,DCM path"]) do |result_text, dcm_result|
@@ -89,17 +82,9 @@ class Identy
   end
 
   def change_string(dcm_result)
-    p dcm_result
     dcm_result.flatten.inject("") do |gather_text, d|
       gather_text + (d + ",")
     end.delete_suffix(",")
-  end
-
-  def gather_nil(targets)
-    targets.inject([]) do |gathered, target|
-      next if target[1] == nil # PID == nil, this file is not DICOM.
-      gathered << target 
-    end
   end
 
   def dbg_chdir(dpath)
