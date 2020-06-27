88b2f832c5d75e21c470674abb73fe0b9e77bf6c
mame-n <nakauchi.s@gmail.com>
2020-06-28 06:37:06 +0900

    dcm_dir is completed

--- identy.rb
+++ identy.rb
@@ -1,38 +1,72 @@
+require "DICOM"
 #require "C:/Program Files (x86)/mybin/patient_name_id"
-require "./patient_name_id"
+#require "./patient_name_id"
 
 class Identy
-  def initialize(dcm_path="./DicomFiles")
-#    print "dcm_path #{dcm_path} -> "
-    @dcm_path = File.expand_path(dcm_path)
-#    puts "#{@dcm_path}"
+  def initialize( argvs )
+    check_argvs( argvs )
+    @results = []
+    @dbg_folder_paths = []
+  end
+
+  def check_argvs( argvs )
+    if argvs.size == 0
+      puts_usage
+      @dcm_folder = nil
+    
+    elsif argvs.size == 1
+      @dcm_folder = argvs[0]
+    end
+  end
+
+
+  def puts_usage
+    puts "No input file path is specified."
+    puts "usage: ruby identy <Folder name>"
   end
 
   def main
-    keep_dir = Dir.pwd
-    @out_file = keep_dir+"/dicom_info.txt"
+    @dcm_path = File.expand_path(@dcm_folder)
+
+#    keep_dir = Dir.pwd
+
+    @out_file = @dcm_path + "/dicom_info.txt"
     puts "Output file #{@out_file}"
 
-    Dir.chdir(@dcm_path)
-    ret = convert_cvs( dcm_dirs )
+#    Dir.chdir(@dcm_path)
+    ret = convert_cvs( dcm_dirs( @dcm_path ).flatten )
     write_cvs( ret )
     Dir.chdir(keep_dir)
     ret
   end
 
-  def dcm_dirs
-    Dir.glob("**/*").map do |file_path|
-      unless File.directory?(file_path)
-#        puts "#{file_path}"
-        dcm = PatientNameID.new("./" + file_path).name_id
-        if dcm
-          [dcm[:pname], dcm[:pid], dcm[:study_date],dcm[:fname],idx(file_path)]
+  attr_accessor :dcm_folder, :results, :dbg_folder_paths
+
+  def dcm_dirs( folder_path )
+
+    Dir.each_child( folder_path ).each do |file_name|
+      puts "File name is #{file_name}"
+      next if file_name == "DICOMDIR"
+
+      file_path = folder_path + "/" + file_name
+
+      if File.file?(file_path)
+        puts "#{file_path}"
+        dcm = DICOM::DObject.read( file_path )
+        if dcm.read?
+          @results << [ dcm["0010,0010"].value, dcm["0010,0020"].value, dcm["0008,0020"].value, folder_path ]
+          puts "Result update #{@results}"
+          return 
         else
-          puts "!!!!!!"
-#          [idx(file_path),nil,nil,nil]
+          # Non DICOM file. Just through this file
         end          
+
+      elsif Dir.exist?(file_path)
+        puts "#{file_path} is folder"; @dbg_folder_paths << file_path
+        dcm_dirs( file_path )
+
       else
-        [nil,nil,nil,nil,idx(file_path)]
+#        [nil,nil,nil,nil,idx(file_path)]
       end
     end
   end
@@ -55,6 +89,7 @@ class Identy
   end
 
   def change_string(dcm_result)
+    p dcm_result
     dcm_result.flatten.inject("") do |gather_text, d|
       gather_text + (d + ",")
     end.delete_suffix(",")
